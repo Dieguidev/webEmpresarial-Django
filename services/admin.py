09249100efae323805263133a47c7887711d7603
Dieguidev <diegogaraycullas@gmail.com>
2023-12-15 22:34:29 -0500

    generando el template de servicios desde la base de datos

--- services/admin.py
+++ services/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+#*Configuracion del panel de administracion
+class ServiceAdmin(admin.ModelAdmin):
+    readonly_fields = ('created','updated')
+
+#Registrando los modelos
+admin.site.register(Service, ServiceAdmin)
