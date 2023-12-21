51fd21c4fff6bfd6e85da2a4b3b59cc2508b195d
Dieguidev <diegogaraycullas@gmail.com>
2023-12-20 21:50:31 -0500

    personalizando el editor de texto en settings.py

--- webEmpresarial/settings.py
+++ webEmpresarial/settings.py
@@ -134,3 +134,16 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+#ckedsitor
+CKEDITOR_CONFIGS = {
+    'default': {
+        'toolbar': 'Custom',
+        'toolbar_Custom': [
+            ['Bold', 'Italic', 'Underline'],
+            ['NumberedList', 'BulletedList', '-', 'Outdent', 'Indent', '-', 'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock'],
+            ['Link', 'Unlink'],
+            ['RemoveFormat', 'Source']
+        ]
+}
+}
